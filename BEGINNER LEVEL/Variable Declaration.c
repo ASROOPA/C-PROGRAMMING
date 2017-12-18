f37bc3c79e896b45b258fc00f088edb9cc2c42cc
ASROOPA <34640686+ASROOPA@users.noreply.github.com>
2017-12-18 12:55:16 +0530

    Create Variable Declaration.c

---
+++ BEGINNER LEVEL/Variable Declaration.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char s1[20]="Sachin Ramesh Tendulkar";
+char s2[20]="Little Master";
+int a=18426;
+int b=463;
+float f=44.83;
+printf("\n%s\n%s\n%d\n%f\n%d",s1,s2,a,f,b);
+}
