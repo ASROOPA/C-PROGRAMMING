9c35b35fc13f24980634fc2c3717c2e6a2039bfd
ASROOPA <34640686+ASROOPA@users.noreply.github.com>
2017-12-18 21:04:19 +0530

    Update vowel.c

--- CODEKATA/vowel.c
+++ CODEKATA/vowel.c
@@ -3,7 +3,6 @@
 int main()
 {
  char s;
- printf("Enter the character");
  scanf("%c",&s);
  if((s=='a')||(s=='e')||(s=='i')||(s=='o')||(s=='u')||(s=='A')||(s=='E')||(s=='I')||(s=='O')||(s=='U'))
  {
