58e66b5512bcf53c0d986acf90d21c5592e83dda
ASROOPA <34640686+ASROOPA@users.noreply.github.com>
2017-12-18 21:02:13 +0530

    Create vowel.c

---
+++ CODEKATA/vowel.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+ char s;
+ printf("Enter the character");
+ scanf("%c",&s);
+ if((s=='a')||(s=='e')||(s=='i')||(s=='o')||(s=='u')||(s=='A')||(s=='E')||(s=='I')||(s=='O')||(s=='U'))
+ {
+     printf("vowel");
+ }
+ else
+ {
+     printf("consonant");
+ }
+}
+
