2bba76b89056211fbbd5514a81c4d1a09af556b5
ASROOPA <34640686+ASROOPA@users.noreply.github.com>
2017-12-18 22:31:09 +0530

    Update sum.c

--- CODEKATA/sum.c
+++ CODEKATA/sum.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+int sum(int num);
 void main()
 {
-    int i,n,sum=0;
+    int i,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    i=sum(n);
+    printf("%d",i);
+}
+int sum(int num)
+{
+    if((num==0 || num==1))
     {
-        sum=sum+i;
+        return num;
     }
-    printf("\n%d",sum);
+    else
+    {
+        return num+sum(num-1);
+    }
 }
 
 
